bac635dc9e8d2baf7b3230e6ab95536fad37180f
Richard Way <thatrickguy@hotmail.com>
2020-03-30 10:06:48 -0500

    Add project files.

---
+++ DeathProjector/Externals/worldometers.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DeathProjector.Externals
+{
+    public class worldometers
+    {
+        private static Dictionary<string, List<WorldometerModel>> cache = new Dictionary<string, List<WorldometerModel>>();
+        private static Dictionary<string, DateTime> cacheDate = new Dictionary<string, DateTime>();
+
+        public static List<WorldometerModel> GetWorldometerData(string Nation)
+        {
+            var lReturn = new List<WorldometerModel>();
+
+            if (Nation == "WI") return GetWisconsin();
+
+            if (!cache.ContainsKey(Nation) ||
+                !cacheDate.ContainsKey(Nation) ||
+                DateTime.Now > cacheDate[Nation].AddHours(1))
+            {
+                string Content;
+                using (WebClient client = new WebClient())
+                {
+                    Content = client.DownloadString("https://www.worldometers.info/coronavirus/country/" + Nation + "/");
+                }
+                var DailyDeathsStart = Content.IndexOf("text: 'Daily Deaths'");
+                var DatesBlockStart = Content.IndexOf("xAxis:", DailyDeathsStart);
+                var DatesStart = Content.IndexOf("[", DatesBlockStart) + 1;
+                var DatesEnd = Content.IndexOf("]", DatesStart);
+                var Dates = StringToDateTimes(Content.Substring(DatesStart, DatesEnd - DatesStart));
+
+                var SeriesStart = Content.IndexOf("series:", DatesEnd);
+                var DataBlockStart = Content.IndexOf("data", SeriesStart);
+                var DataStart = Content.IndexOf("[", DataBlockStart) + 1;
+                var DataEnd = Content.IndexOf("]", DataStart);
+                var Values = StringToInts(Content.Substring(DataStart, DataEnd - DataStart));
+
+                if (Dates.Count != Values.Count) throw new DataMisalignedException("Dates (" + Dates.Count + ") and Values (" + Values.Count + ") missmatch!");
+                for (int i = 0; i < Dates.Count; i++)
+                {
+                    lReturn.Add(new WorldometerModel() { Date = Dates[i], Deaths = Values[i] });
+                }
+                cache[Nation] = lReturn;
+                cacheDate[Nation] = DateTime.Now;
+            }
+            else
+            {
+                lReturn = cache[Nation];
+            }
+            return lReturn;
+        }
+
+        /// <summary>
+        /// Converts from a coma delimited list of enclosed string date parts to a List of DateTimes
+        /// </summary>
+        /// <param name="input">A coma separated list of enclosed string date parts ("Feb 1","Feb 2",...)</param>
+        /// <returns>A list of DateTimes</returns>
+        private static List<DateTime> StringToDateTimes(string input)
+        {
+            var lReturn = new List<DateTime>();
+
+            var sdates = input.Split(",");
+            foreach (var sdate in sdates)
+            {
+                var s = sdate.Trim().TrimStart('\"').TrimEnd('\"') + ", 2020";
+                lReturn.Add(DateTime.Parse(s));
+            }
+
+            return lReturn;
+        }
+
+        /// <summary>
+        /// Takes a coma delimited list of integers and returns a list of ints
+        /// </summary>
+        /// <param name="input">A coma separated list of ints (1,2,3,4,5,...)</param>
+        /// <returns>A list of ints</returns>
+        private static List<int> StringToInts(string input)
+        {
+            var lReturn = new List<int>();
+
+            var sints = input.Split(",");
+            foreach (var sint in sints)
+            {
+                lReturn.Add(int.Parse(sint.Trim()));
+            }
+
+            return lReturn;
+        }
+
+        private static List<WorldometerModel> GetWisconsin()
+        {
+            var lReturn = new List<WorldometerModel>();
+
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 19, 0, 0, 0, 0), Deaths = 1 });
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 20, 0, 0, 0, 0), Deaths = 1 });
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 21, 0, 0, 0, 0), Deaths = 0 });
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 22, 0, 0, 0, 0), Deaths = 1 });
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 23, 0, 0, 0, 0), Deaths = 1 });
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 24, 0, 0, 0, 0), Deaths = 1 });
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 25, 0, 0, 0, 0), Deaths = 2 });
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 26, 0, 0, 0, 0), Deaths = 2 });
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 27, 0, 0, 0, 0), Deaths = 7 });
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 28, 0, 0, 0, 0), Deaths = 1 });
+            lReturn.Add(new WorldometerModel() { Date = new DateTime(2020, 3, 29, 0, 0, 0, 0), Deaths = 1 });
+
+            return lReturn;
+        }
+    }
+
+    public class WorldometerModel
+    {
+        public DateTime Date { get; set; }
+        public int Deaths { get; set; }
+    }
+}
---
+++ DeathProjector/Services/COVID19Service.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeathProjector.Services
+{
+    public class COVID19Service
+    {
+        public static List<OutputModel> GenerateModel(string Nation, int DaysTillDeath, double DaysTillDouble, 
+                                         double CaseFatalityRate, int MaxInfection, DateTime TargetDate, int DeathsOnTargetDate, 
+                                         DateTime SocialDistancingDate, double SocialDistancingDoublingRate,
+                                         DateTime ShelterInPlaceDate , double ShelterInPlaceDoublingRate,
+                                         DateTime LockdownDate, double LockdownDoublingRate,
+                                         int ProjectionLength)
+        {
+            var lReturn = new List<OutputModel>();
+
+            // Get history from Worldometer
+            var History = DeathProjector.Externals.worldometers.GetWorldometerData(Nation);
+
+            // Set initial data
+            var doubleRate = (Math.Exp(Math.Log(2) / DaysTillDouble) - 1);
+            var CurrentDate = TargetDate.AddDays(-1 * DaysTillDeath);
+            var CurrentInfected = 0;
+            var NewInfections = Math.Round(DeathsOnTargetDate / (CaseFatalityRate / 100), 0);
+
+            // Set the first day
+            var id = new OutputModel();
+            id.Date = CurrentDate;
+            id.ProjectedDeaths = 0;
+            id.ProjectedInfections = (int)NewInfections;
+            CurrentInfected = (int)Math.Round(id.ProjectedInfections / (doubleRate), 0);
+            id.TotalProjectedInfections = (int)Math.Round(id.ProjectedInfections / (doubleRate), 0);
+            lReturn.Add(id);
+
+            // Prep for looping!
+            CurrentInfected += (int)NewInfections;
+            CurrentDate = CurrentDate.AddDays(1);
+
+            while (CurrentDate < TargetDate.AddDays(ProjectionLength + 1))
+            {
+                // update the doubleRate appropriately
+                if (CurrentDate >= SocialDistancingDate) doubleRate = (Math.Exp(Math.Log(2) / SocialDistancingDoublingRate) - 1);
+                if (CurrentDate >= ShelterInPlaceDate) doubleRate = (Math.Exp(Math.Log(2) / ShelterInPlaceDoublingRate) - 1);
+                if (CurrentDate >= LockdownDate) doubleRate = (Math.Exp(Math.Log(2) / LockdownDoublingRate) - 1);
+
+                id = new OutputModel();
+                id.Date = CurrentDate.Date;
+                
+                // Get projected deaths based off of how many new people got sick DaysTillDeath days ago
+                id.ProjectedDeaths = 0;
+                var SicknessDate = (from p in lReturn where p.Date == CurrentDate.AddDays(-1 * DaysTillDeath) select p).FirstOrDefault();
+                if (SicknessDate != null) id.ProjectedDeaths = (int)( SicknessDate.ProjectedInfections * (CaseFatalityRate / 100));
+                
+                id.TotalProjectedInfections = (int)Math.Round(CurrentInfected * (1 + doubleRate), 0);
+                id.ProjectedInfections = id.TotalProjectedInfections - CurrentInfected;
+
+                NewInfections = id.ProjectedInfections;
+                CurrentInfected = id.TotalProjectedInfections;
+
+                // Correct for max infection
+                if (CurrentInfected > MaxInfection)
+                {
+                    CurrentInfected = MaxInfection;
+                    NewInfections = 0;
+                    id.TotalProjectedInfections = MaxInfection;
+                    id.ProjectedInfections = 0;
+                }
+
+                // Get Reported Deaths
+                var q = (from p in History where p.Date.Date.ToShortDateString() == CurrentDate.ToShortDateString() select p).FirstOrDefault();
+                if (q!= null)
+                {
+                    id.ReportedDeaths = q.Deaths;
+                }
+
+                lReturn.Add(id);
+                CurrentDate = CurrentDate.AddDays(1);
+            }
+
+            return AggregateTotals(lReturn);
+        }
+
+        private static List<OutputModel> AggregateTotals(List<OutputModel> input)
+        {
+            var lReturn =input;
+
+            int TotalProjectedDeaths=0;
+            int TotalProjectedInfections=0;
+            int TotalReportedDeaths=0;
+            foreach (var om in input)
+            {
+                TotalProjectedDeaths += om.ProjectedDeaths;
+                TotalProjectedInfections += om.ProjectedInfections;
+                TotalReportedDeaths += om.ReportedDeaths;
+
+                om.TotalProjectedDeaths = TotalProjectedDeaths;
+                om.TotalProjectedInfections = TotalProjectedInfections;
+                om.TotalReportedDeaths = TotalReportedDeaths;
+            }
+
+            return lReturn;
+        }
+    }
+
+    public class OutputModel
+    {
+        public DateTime Date { get; set; }
+        public int ProjectedInfections { get; set; }
+        public int TotalProjectedInfections { get; set; }
+        public int ProjectedDeaths { get; set; }
+        public int TotalProjectedDeaths { get; set; }
+        public int ReportedDeaths { get; set; }
+        public int TotalReportedDeaths { get; set; }
+    }
+}
